503521d20f128a4719bacbb71ed6ff3ef06bcfdb
lucas weidner <lucasmweidner@gmail.com>
2025-02-27 22:07:52 -0300

    Ataulização da aba de cadastro do produto HTML e JS

---
+++ scripts/produto/exemplo-listar-todos.js
@@ -0,0 +1,9 @@
+let tabelaProdutos = document.getElementById ("tebala-produtos");
+let botaoConsultarProdutors = document.getElementById ("consultar-produtos");
+
+let urlAPI = "https://public.franciscosensaulas.com"
+
+function atribuirCliqueBotoesApagar () {
+
+    let botoesApagar = document.getElementsByClassName("botao-apagar");
+}
